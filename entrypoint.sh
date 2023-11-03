3bd8d830141f66d8feaaa3f9803caad9ad1e8487
olga-cs <olga.daminova@chainstack.com>
2023-11-03 14:48:31 +0100

    change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Start the agent
-/var/ossec/bin/ossec-control start
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start agent: $status"
-  exit $status
-fi
-
-echo "background jobs running, listening for changes"
-
-while sleep 60; do
-  /var/ossec/bin/ossec-control status > /dev/null 2>&1
-  status=$?
-  if [ $status -ne 0 ]; then
-    echo "looks like the agent died."
-    exit 1
-  fi
-done
+sudo systemctl daemon-reload
+sudo systemctl enable wazuh-agent
+sudo systemctl start wazuh-agent
